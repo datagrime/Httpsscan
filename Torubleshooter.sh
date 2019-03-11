a02e5cbe158af412c34a8dc4e146ecf1448971e8
datagrime <7754841+datagrime@users.noreply.github.com>
2019-03-11 10:30:21 -0400

    Update Torubleshooter.sh

--- Torubleshooter.sh
+++ Torubleshooter.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 checkdomain=ubuntu.com
-Purp='\033[0;35m'
-NC='\033[0m'
 
 #Check if local network is up
 if ping -q -c 1 -W 1 google.com >/dev/null; then
-  echo -e "${Purp}The network is up"
+  echo -e "\033[1mThe network is up"
 else
-  echo -e "${Purp}The network is down"
+  echo -e "\033[1mThe network is down"
 fi
 
 
 #Scan HTTPS on port 443 
 if nc -zw1 $checkdomain 443; then
-  echo -e "${Purp}Port scan 443 succesfull, we have connectivity"
+  echo -e "\033[1mPort scan 443 succesfull, we have connectivity"
 else
-  echo -e "${Purp}Port scan 443 failed, no connectivity "
+  echo -e "\033[1mPort scan 443 failed, no connectivity "
 
 fi
 
 
 #Check and Display IP Tables 
-echo -e "Display Ip Tables"
+echo -e "\033[1mDisplay Ip Tables"
 
 iptables --list
 
